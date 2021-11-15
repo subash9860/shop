17a0b643c6310a914eb5ea3894d01c82490512d3
subash kc <subaazkc@gmail.com>
2021-11-15 09:28:15 +0545

    post in firebase

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,45 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 import './product.dart';
 
 class Products with ChangeNotifier {
   final List<Product> _items = [
     Product(
-        id: 'p1',
-        title: 'Red Shirt',
-        description: 'A red shirt - it is pretty red!',
-        price: 29.99,
-        imageUrl: 
-        // 'assets/burger.jpg'
-        'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
-        ),
+      id: 'p1',
+      title: 'Red Shirt',
+      description: 'A red shirt - it is pretty red!',
+      price: 29.99,
+      imageUrl:
+          // 'assets/burger.jpg'
+          'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
+    ),
     Product(
-        id: 'p2',
-        title: 'Trousers',
-        description: 'A nice pair of trousers.',
-        price: 59.99,
-        imageUrl: 
-        // 'assets/coffee.jpg'
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
-        ),
+      id: 'p2',
+      title: 'Trousers',
+      description: 'A nice pair of trousers.',
+      price: 59.99,
+      imageUrl:
+          // 'assets/coffee.jpg'
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
+    ),
     Product(
-        id: 'p3',
-        title: 'Yellow Scarf',
-        description: 'Warm and cozy - exactly what you need for the winter.',
-        price: 19.99,
-        imageUrl:
-        //  'assets/dessert.jpg'
-        'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
-        ),
+      id: 'p3',
+      title: 'Yellow Scarf',
+      description: 'Warm and cozy - exactly what you need for the winter.',
+      price: 19.99,
+      imageUrl:
+          //  'assets/dessert.jpg'
+          'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
+    ),
     Product(
-        id: 'p4',
-        title: 'A Pan',
-        description: 'Prepare any meal you want.',
-        price: 49.99,
-        imageUrl: 
-        // 'assets/pizza.jpg'
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
-        ),
+      id: 'p4',
+      title: 'A Pan',
+      description: 'Prepare any meal you want.',
+      price: 49.99,
+      imageUrl:
+          // 'assets/pizza.jpg'
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
+    ),
   ];
   // var _showFavoritesOnly = false;
 
@@ -55,7 +57,8 @@ class Products with ChangeNotifier {
   }
 
   Product findById(String id) {
-    return _items.firstWhere((prod) => prod.id == id);
+    return _items.firstWhere((element) => element.id == id);
+    // _items.firstWhereOrNull((index, element) => e)((prod) => prod.id == id);
   }
 
   // void showFavoritesOnly() {
@@ -69,20 +72,37 @@ class Products with ChangeNotifier {
   // }
 
   void addProduct(Product product) {
-    final newProduct = Product(
-        id: DateTime.now().toString(),
-        title: product.title,
-        description: product.description,
-        price: product.price,
-        imageUrl: product.imageUrl);
-    _items.add(newProduct);
-    // _items.insert(0, newProduct); // for start of the list
-    notifyListeners();
+    var url = Uri.parse(
+        'https://shop-20ff4-default-rtdb.asia-southeast1.firebasedatabase.app/product.json');
+    http
+        .post(url,
+            body: json.encode({
+              'title': product.title,
+              'descrition': product.description,
+              'imageUrl': product.imageUrl,
+              'price': product.price,
+              'isFavorite': product.isFavorite,
+            }))
+        .then((response) {
+      // print(json.decode(response.body)['name']);
+      final newProduct = Product(
+          id: json.decode(response.body)['name'],
+          title: product.title,
+          description: product.description,
+          price: product.price,
+          imageUrl: product.imageUrl);
+      _items.add(newProduct);
+      // _items.insert(0, newProduct); // for start of the list
+      notifyListeners();
+    });
   }
 
   void updateProduct(String id, Product newProduct) {
     final proIndex = _items.indexWhere((element) => element.id == id);
-    _items[proIndex] = newProduct;
+    if (proIndex > 0 && !proIndex.isNaN) {
+      _items[proIndex] = newProduct;
+    }
+
     notifyListeners();
   }
 
